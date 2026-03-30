13ce0b32e3a90dcf12a243f6169526351c87b328
Nasir Davis <ndavis67@ut.utm.edu>
2026-03-30 10:43:21 -0500

    completed game.h file

---
+++ game.h
@@ -0,0 +1,15 @@
+#ifndef GAME_H
+#define GAME_H
+#include <string>
+
+struct Game{
+  std::string title;
+  std::string publisher;
+  std::string genre;
+  float hours_played;
+  float price;
+  short year_released;
+
+};
+
+#endif
